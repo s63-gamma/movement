f446559e54766e976082a74c4543d9a6e1b8df15
requinard <d.diks94@gmail.com>
2017-02-23 13:52:20 +0100

    feat(hibernate): add trip entity
    Add trip entity to pull from database
    
    Change-Id: I380a2e2f482837a2abbe58dcce2922001d38b5bd

---
+++ src/main/java/com/gamma/dal/entities/Trip.kt
@@ -0,0 +1,31 @@
+package com.gamma.dal.entities
+
+import org.hibernate.annotations.GenericGenerator
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.Id
+
+/**
+ * Represent Trip table
+ */
+@Entity
+class Trip {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @Column(unique = true)
+    val uuid: String = ""
+
+    var startPoint: Double = 0.0
+    var endPoint: Double = 0.0
+    var totalDistance: Double = 0.0
+
+    constructor(startPoint: Double, endPoint: Double, totalDistance: Double) {
+        this.startPoint = startPoint
+        this.endPoint = endPoint
+        this.totalDistance = totalDistance
+    }
+
+    constructor()
+}
