76da192b714b9d04880409d72852b3cf36a4d362
Rick Rongen <rickrongen@outlook.com>
2017-03-07 12:37:35 +0100

    start of CarController Start of Repository

--- src/main/java/com/gamma/GMovementApplication.java
+++ src/main/java/com/gamma/GMovementApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+//@SpringBootApplication(scanBasePackages = {"com.gamma.Controller"})
 @SpringBootApplication
 public class GMovementApplication extends SpringBootServletInitializer {
 
