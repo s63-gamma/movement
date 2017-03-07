9fe8ef390b4f287ddd3e7499265de99e6d196ed3
Guus Hamm <guushamm@gmail.com>
2017-03-07 16:27:41 +0100

    feat(): add gpspoint crud repository
    feat: add region repository

---
+++ src/test/java/com/gamma/dal/entities/RegionTest.kt
@@ -0,0 +1,29 @@
+package com.gamma.dal.entities
+
+import com.gamma.repository.RegionRepository
+import org.junit.Assert.assertNotNull
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+/**
+ * Created by requinard on 2/21/17.
+ */
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class RegionTest {
+    @Autowired
+    lateinit var regionRepository: RegionRepository
+
+    @Test
+    @Throws(Exception::class)
+    fun createRegion() {
+        val region = Region("MANC UNITED", 1, 1, 1, 1)
+        regionRepository.save(region)
+
+        assertNotNull(region.uuid)
+    }
+
+}
