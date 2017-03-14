9633f548b8b22572f9432d624e27dc2cb551423c
Guus Hamm <guushamm@gmail.com>
2017-03-14 10:46:47 +0100

    feat: add trip repository
    Change-Id: I806b5be968ae0dca37d5efb2489454865eb074d5

--- src/main/java/com/gamma/repository/TripRepository.java
+++ src/main/java/com/gamma/repository/TripRepository.java
@@ -1,11 +1,11 @@
 package com.gamma.repository;
 
-import com.gamma.dal.entities.Region;
+import com.gamma.dal.entities.Trip;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.UUID;
 
-@RepositoryRestResource(collectionResourceRel = "region", path = "region")
-public interface RegionRepository extends JpaRepository<Region, UUID> {
+@RepositoryRestResource(collectionResourceRel = "trip", path = "trip")
+public interface TripRepository extends JpaRepository<Trip, UUID> {
 }
