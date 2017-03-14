1b3c4370600b39f991f01d86f343651f729f78b9
Guus Hamm <guushamm@gmail.com>
2017-03-14 12:39:48 +0100

    fix: fix region repository
    Change-Id: Id00b970b1edea2b035114dff4136f241063863a8

--- src/main/java/com/gamma/repository/RegionRepository.java
+++ src/main/java/com/gamma/repository/RegionRepository.java
@@ -1,7 +1,11 @@
 package com.gamma.repository;
 
-/**
- * Created by guushamm on 14-3-17.
- */
-public class RegionRepository {
+import com.gamma.dal.entities.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.UUID;
+
+@RepositoryRestResource(collectionResourceRel = "region", path = "region")
+public interface RegionRepository extends JpaRepository<Region, UUID> {
 }
