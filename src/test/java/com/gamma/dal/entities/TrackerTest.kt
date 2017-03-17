2511d6ef3f27fa4b46d629a4c82b94c1eab6038b
Rick Rongen <rickrongen@outlook.com>
2017-03-17 13:00:55 +0100

    fix: Fixed TrackerTest test. Fixed GpsPointTest, made sure it flushed jenkins test message

--- src/test/java/com/gamma/dal/entities/TrackerTest.kt
+++ src/test/java/com/gamma/dal/entities/TrackerTest.kt
@@ -1,24 +1,27 @@
 package com.gamma.dal.entities
 
-import com.gamma.dal.util.DatabaseUtil
-import com.gamma.dal.util.DatabaseUtilTest
+import com.gamma.repository.TrackerRepository
 import org.junit.Assert.*
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
 
 /**
  * Created by requinard on 2/23/17.
  */
+@RunWith(SpringRunner::class)
+@SpringBootTest
 class TrackerTest{
+    @Autowired
+    lateinit var trackerRepository: TrackerRepository
+
     @Test
     fun create(){
-        val session = DatabaseUtil().getSession()
-        session.beginTransaction()
-
         val tracker = Tracker(1, 2, "hopsakee")
-        session.save(tracker)
 
-        session.transaction.commit()
-        session.close()
+        trackerRepository.save(tracker)
 
         assertNotNull(tracker.uuid)
     }
