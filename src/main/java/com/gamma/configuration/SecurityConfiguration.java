ca6f1d21ebd539e1d8125c82480106a342c4ea0e
Guus Hamm <guushamm@gmail.com>
2017-04-04 15:39:23 +0200

    feat(SecurityConfig): allow csrf
    Disable csrf for use with Angular2 frontend. NOTE TO SELF if hacked this is reason.
    
    Change-Id: I21238c9c27f1695f2dea99a82f3046291280ae08

--- src/main/java/com/gamma/configuration/SecurityConfiguration.java
+++ src/main/java/com/gamma/configuration/SecurityConfiguration.java
@@ -25,7 +25,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-				.addFilterBefore(new CorsFilter(corsConfigurationSource()), ChannelProcessingFilter.class);
+			.addFilterBefore(new CorsFilter(corsConfigurationSource()), ChannelProcessingFilter.class)
+			.csrf().disable();
 	}
 
 	@Bean
