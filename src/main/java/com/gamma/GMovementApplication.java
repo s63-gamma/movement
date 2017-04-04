826e247a89b8c4db1c0245f7c25881b807847ddb
Guus Hamm <guushamm@gmail.com>
2017-04-04 09:25:38 +0200

    feat: add commandlinerunnner to insert data at startup
    Change-Id: I984a93861986a3c2dd56ea3e009aa238e6ca127f

--- src/main/java/com/gamma/GMovementApplication.java
+++ src/main/java/com/gamma/GMovementApplication.java
@@ -1,9 +1,18 @@
 package com.gamma;
 
+import com.gamma.dal.entities.Car;
+import com.gamma.dal.entities.Invoice;
+import com.gamma.repository.CarRepository;
+import com.gamma.repository.InvoiceRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 @SpringBootApplication
 public class GMovementApplication extends SpringBootServletInitializer {
@@ -16,5 +25,27 @@ public class GMovementApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(GMovementApplication.class, args);
 	}
+
+	@Bean
+	public CommandLineRunner demo(InvoiceRepository invoiceRepository, CarRepository carRepository) {
+		return (args) -> {
+			ArrayList<Invoice> invoices = new ArrayList<>();
+			invoices.add(new Invoice(new Date(), 0, 200, 0, "Hallo"));
+			invoices.add(new Invoice(new Date(), 0, 40, 1, "Hallo"));
+			invoices.add(new Invoice(new Date(), 0, 600, 0, "Hallo"));
+			invoices.add(new Invoice(new Date(), 0, 275, 1, "Hallo"));
+			invoices.add(new Invoice(new Date(), 0, 500, 0, "Hallo"));
+			invoices.add(new Invoice(new Date(), 0, 25, 1, "Hallo"));
+			invoices.add(new Invoice(new Date(), 0, 245, 1, "Hallo"));
+			invoices.add(new Invoice(new Date(), 0, 365, 0, "Hallo"));
+
+			invoices.forEach(invoiceRepository::save);
+
+			ArrayList<Car> cars = new ArrayList<>();
+			cars.add(new Car(1994,"NE-UK-69",20000.0,420.0, "Sedan"));
+			cars.forEach(carRepository::save);
+		};
+	}
+
 
 }
