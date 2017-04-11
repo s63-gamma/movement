c9762d375226da488000251e3c3645db6305e9f0
Guus Hamm <guushamm@gmail.com>
2017-04-11 09:58:40 +0200

    feat: add ability to retrieve regions based on size
    In the front-end regions should be displayed based on size,
    hence this method.
    Change-Id: I3ac32392235081cb6cf5309a8c581525418acdb9

--- src/main/java/com/gamma/repository/RegionRepository.java
+++ src/main/java/com/gamma/repository/RegionRepository.java
@@ -4,8 +4,10 @@ import com.gamma.dal.entities.Region;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
 import java.util.UUID;
 
 @RepositoryRestResource(collectionResourceRel = "region", path = "region")
 public interface RegionRepository extends JpaRepository<Region, UUID> {
+	List<Region> findAllByOrderByRadiusDesc();
 }
